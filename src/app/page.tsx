94fcc95cef14684212c863b2ba9a6d348cbe207f
Rishabh Yadav <github.for.rishabh@gmail.com>
2023-09-03 17:37:26 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+"use client";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <div className="min-h-screen bg-[#1e293b] py-10">
+     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      {/* imagee logo */}
+      
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+          <span className="block">Welcome to</span>
+          <span className="block text-indigo-500">RiderSync Hub</span>
+        </h1> 
+        </div>
+    </div>
+    {/* header */}
+    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div className="absolute inset-0 bg-gradient-to-r from-[#b469ff] to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl">
+      </div>
+
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="max-w-md mx-auto">
+          <div>
+            <img src="/logo-full.png" className="h-20 sm:h-15" />
+          </div>
+          <div className="divide-y divide-gray-200">
+            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <p>
+              RiderSync Hub is your all-in-one platform for connecting with local travel enthusiasts who are planning trips to the same destination, regardless of their chosen mode of transportation.
+              </p>
+              <ul className="list-disc space-y-2">
+                <h2 className="font-bold">Key Feature: </h2>
+                <li className="flex items-start">
+                  <span className="h-6 flex items-center sm:h-7">
+                   <img src="/bycicle.png" className="h-6 w-6" />
+                  </span>
+                  <p className="ml-2">
+                    <code className="text-sm font-bold text-gray-900">Route Sharing</code>
+                  </p>
+                </li> 
+                <li className="flex items-start">
+                  <span className="h-6 flex items-center sm:h-7">
+                   <img src="/bycicle.png" className="h-6 w-6" />
+                  </span>
+                  <p className="ml-2">
+                    <code className="text-sm font-bold text-gray-900">Cost Sharing</code> 
+                  </p>
+                </li> 
+                <li className="flex items-start">
+                  <span className="h-6 flex items-center sm:h-7">
+                   <img src="/bycicle.png" className="h-6 w-6" />
+                  </span>
+                  <p className="ml-2">
+                    
+                    <code className="text-sm font-bold text-gray-900">Community Support</code> 
+                  </p>
+                </li> 
+                
+                <li className="flex items-start">
+                  <span className="h-6 flex items-center sm:h-7">
+                   <img src="/bycicle.png" className="h-6 w-6" />
+                  </span>
+                  <p className="ml-2">
+                    
+                    <code className="text-sm font-bold text-gray-900">Safety First</code> 
+                  </p>
+                </li> 
+              </ul>
+              <p>
+              RiderSync Hub brings the community together for shared adventures and cost-effective journeys.
+              </p>  
+            </div>
+            <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+              <p>Are you ready to Join the Ride?</p>
+              <p>
+                <a href="#" className="text-blue-500 hover:text-blue-600">Get started</a>
+              </p>
+
+            </div>
+          </div>
         </div>
       </div>
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+   
+  </div>
+  );
 }
