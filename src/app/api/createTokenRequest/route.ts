d2370fa8485ebfa76914ef184c7ce2f728afdd22
Rishabh Yadav <github.for.rishabh@gmail.com>
2023-10-27 21:09:43 +0530

    token api file deleted

--- src/app/api/createTokenRequest/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import Ably from 'ably/promises';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-  res.status(200).json(tokenRequestData);
-}
